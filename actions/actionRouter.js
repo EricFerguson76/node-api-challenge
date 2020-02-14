e7cbc4c42dee53ab373356068917a3e1cdbca59f
Eric Ferguson <turtle0888@gmail.com>
2020-02-14 12:04:15 -0600

    Add GET, PUT, DELETE to actions

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -4,4 +4,44 @@ const router = express.Router();
 
 router.use(express.json());
 
+const Actions = require('../data/helpers/actionModel');
+
+router.get('/:id', (req, res) => {
+  Actions.get(req.params.id)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(() => {
+      rex.status(500).json({ message: 'Error retreiving actions' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Actions.remove(req.param.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The action has been destroyed' });
+      } else {
+        res.status(404).json({ message: 'The action could not be found' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Error removing the action' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Actions.update(req.params.id, req.body)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({ message: 'The action could not be found' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Error updating the action' });
+    });
+});
+
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -8,8 +8,18 @@ const server = express();
 server.use('/api/projects', projectRouter);
 server.use('/api/actions', actionRouter);
 
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>My Sprint Challenge Project<h2>`);
 });
+
+//Middleware
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.originalUrl} `
+  );
+  next();
+}
 
 module.exports = server;
