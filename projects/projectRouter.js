93735101cb22261bef62bdf3af399661d6ba9f4e
Eric Ferguson <turtle0888@gmail.com>
2020-02-14 13:07:54 -0600

    Add PUT and DELETE to projects

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -61,4 +61,32 @@ router.post('/:id/actions', (req, res) => {
     });
 });
 
+router.put('/:id', (req, res) => {
+  Projects.update(req.params.id, req.body)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: 'The project could not be found' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Error updating the project' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Projects.remove(req.param.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The project has been destroyed' });
+      } else {
+        res.status(404).json({ message: 'The project could not be found' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Error removing the project' });
+    });
+});
+
 module.exports = router;
